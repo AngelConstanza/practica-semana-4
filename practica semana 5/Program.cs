13f8118d9b429e40b4ef3228d8c584e07eef837c
LUIS ANGEL ESCOBAR CONSTANZA <SMSS144922@ugb.edu.sv>
2022-08-29 22:20:19 -0300

    Agregar archivos de proyecto.

---
+++ practica semana 5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practica_semana_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
